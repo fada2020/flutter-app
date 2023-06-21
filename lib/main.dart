760879473fbdf7441eec14fd5864a8f04ef6621a
fada2020 <hyeogjui4@gmail.com>
2023-06-21 15:49:36 +0900

    기초 바텀 바 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
-import 'package:ex_f/screen/home_screen.dart';
+import 'package:ex_f/widget/bottom_bar.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-      const MaterialApp(
-        home: HomeScreen(),
-      ),
-  );
-}
+void main() => runApp(MyApp());
+class MyApp extends StatefulWidget {
+  _MyAppState createState()=> _MyAppState();
 
+}
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'BbongFlix',
+    theme: ThemeData(
+      brightness: Brightness.dark,
+    primaryColor: Colors.black,
+    hintColor: Colors.white,
+    ),
+      home: DefaultTabController(length: 4, child: Scaffold(body: TabBarView(physics: NeverScrollableScrollPhysics(), children: <Widget>[
+      Container(child: Center(child: Text('home'),),),
+      Container(child: Center(child: Text('search'),),),
+      Container(child: Center(child: Text('saved'),),),
+      Container(child: Center(child: Text('more'),),),
+      ] ,
+      ),
+        bottomNavigationBar: Bottom( ),
+      ),
+      ),
+    );
+
+  }
+}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,31 +1,16 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  int number = 1;
+class _HomeScreenState extends State<HomeScreen>{
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: GestureDetector(
-          onTap: (){
-            final newNumber = Random().nextInt(10) + 1;
-            setState(() {
-              number = newNumber;
-            });
-          },
-          child: Image.asset('assets/img/$number.jpeg'),
-        ),
-      ),
-    );
+  void initState(){
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context){
+    return Container(child: Center(child: Text('home'),),);
   }
 }
