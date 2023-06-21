a766016619aa460b2dd9a697cf8c745ed4e3914c
fada2020 <hyeogjui4@gmail.com>
2023-06-21 15:51:22 +0900

    option + command + L

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,50 @@
+import 'package:ex_f/screen/home_screen.dart';
 import 'package:ex_f/widget/bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-class MyApp extends StatefulWidget {
-  _MyAppState createState()=> _MyAppState();
 
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
 }
+
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'BbongFlix',
-    theme: ThemeData(
-      brightness: Brightness.dark,
-    primaryColor: Colors.black,
-    hintColor: Colors.white,
-    ),
-      home: DefaultTabController(length: 4, child: Scaffold(body: TabBarView(physics: NeverScrollableScrollPhysics(), children: <Widget>[
-      Container(child: Center(child: Text('home'),),),
-      Container(child: Center(child: Text('search'),),),
-      Container(child: Center(child: Text('saved'),),),
-      Container(child: Center(child: Text('more'),),),
-      ] ,
+    return MaterialApp(
+      title: 'BbongFlix',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        hintColor: Colors.white,
       ),
-        bottomNavigationBar: Bottom( ),
-      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              HomeScreen(),
+              Container(
+                child: Center(
+                  child: Text('search'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('saved'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('more'),
+                ),
+              ),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
       ),
     );
-
   }
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -4,13 +4,18 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>{
+class _HomeScreenState extends State<HomeScreen> {
   @override
-  void initState(){
+  void initState() {
     super.initState();
   }
+
   @override
-  Widget build(BuildContext context){
-    return Container(child: Center(child: Text('home'),),);
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('home'),
+      ),
+    );
   }
 }
